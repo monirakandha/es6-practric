5e874e85842267922f49cf4fc3053d819e662486
monirhosenakanda <monirhosenakanda@gmail.com>
2022-02-21 09:53:02 +0600

    let vs const solve

---
+++ practric.js
@@ -0,0 +1,10 @@
+// ১. কখন const আর কখন let দিয়ে ভেরিয়েবল ডিক্লেয়ার করতে হয় সেটা তোমাকে জানতেই হবে। তুমি নিজে নিজে একটা const দিয়ে আরেকটা let দিয়ে ভেরিয়েবল ডিক্লেয়ার করে ফেলো।
+
+const phoneNumber = 01719473072;
+console.log(typeof(phoneNumber));
+
+// let phoneModel = 'Samsung S20';
+// console.log(phoneModel);
+let phoneModel = 'Iphone12 pro max';
+console.log(phoneModel);
+
